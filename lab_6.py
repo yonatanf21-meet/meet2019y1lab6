369c5f1ee4667ce8e97715cc27312f0bef966d79
yonatanf21-meet <yonatanf21@meet.mit.edu>
2019-07-28 11:26:33 -0400

    Stopping here

--- lab_6.py
+++ lab_6.py
@@ -8,3 +8,16 @@ while x<300:
 turtle.mainloop()
 
 
+total = 0
+for number in range(1, 10 + 1):
+    print (number)
+    total = total + number
+print(total)
+
+def add_number():
+    for number in range(3, 13 + 2):
+        print(number)
+        add_number = add_number + number
+print(add_number)
+
+
