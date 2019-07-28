52185c799fc264b58aa181d020e5eab02d15858b
yonatanf21-meet <yonatanf21@meet.mit.edu>
2019-07-28 12:59:55 -0400

    Stopping here

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(n):
+  
+    result = []
+    for count in range(1, n):
+        if count % 3 == 0:
+            result.append("Fizz")
+        else:
+             result.append(count)
+
+    print(count)
+
+fizzbuzz = 10
+
+ 
+         
--- lab_6.py
+++ lab_6.py
@@ -1,23 +1,21 @@
+
+         
 import turtle
-x = 0
-while x<300:
-    y = x**2/300 #x**2 is the same as x*x
-    turtle.goto(x,y)
-    x = x + 100
-
-turtle.mainloop()
-
-
-total = 0
-for number in range(1, 10 + 1):
-    print (number)
-    total = total + number
-print(total)
-
-def add_number():
-    for number in range(3, 13 + 2):
-        print(number)
-        add_number = add_number + number
-print(add_number)
-
+turtle.tracer(1)
+rounds = 10
+size = 10
+mike = turtle.clone()
+steve = turtle.clone()
+turtle.bgcolor("white")
+turtle.hideturtle()
+mike.color("gold")
+steve.color("blue")
+steve.goto(5,5)
+while True:
+    mike.forward(size)
+    mike.left(90)
+    steve.forward(-size)
+    steve.left(-90)
+    size += 10
+turtle.mainloop
 
