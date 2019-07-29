ca15f36822aacdaa04d6a21ad9e901910eac85b2
yonatanf21-meet <yonatanf21@meet.mit.edu>
2019-07-29 07:23:03 -0400

    Stopping here

---
+++ functions.py
@@ -0,0 +1,15 @@
+def add_numbers():
+    
+    total = 0
+    for number in range(1, 10 + 1):
+        print(number)
+total = total + number
+        print(total)
+        answer = add_numbers()
+pirnt(answer)
+
+
+
+
+def add_numbers(start, end):
+    
